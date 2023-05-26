7fb2410973ad3923b542f3fa2497c20e589d389d
Tobias Krainhöfner <tobikrain@gmail.com>
2023-05-26 11:26:52 +0200

    nick du hund

--- run.py
+++ run.py
@@ -1,33 +1,33 @@
-"""Example of getting a direct reading from RPi.GPIO."""
-
-import RPi.GPIO as GPIO
 from hcsr04sensor import sensor
 
-# This script uses a static method inside the Measurement class
-# called basic_distance
-# No median readings pulled from a sample for error correction
-# No setmode in the library
-# No pin cleanups.  You handle all of these things in your own code
-# Just a simple return of a cm distance as reported directly from Rpi.GPIO
-# Only returns a metric value.
+# Created by Al Audet
+# MIT License
 
-# set gpio pins
-trig = 17
-echo = 27
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)  # use GPIO.BOARD for board pin values
-x = sensor.Measurement
-# use default temp of 20 Celcius
-distance_warm = x.basic_distance(trig, echo)
+def main():
+    """Calculate the distance of an object in centimeters using a HCSR04 sensor
+       and a Raspberry Pi"""
 
-# example of passing temperature reading
-# temperature affects speed of sound
-# Easily combine with a temperature sensor to pass the current temp
-temp = -30
-distance_cold = x.basic_distance(trig, echo, celsius=temp)
+    trig_pin = 17
+    echo_pin = 27
+    # Default values
+    # unit = 'metric'
+    # temperature = 20 (room temp in Celsius)
 
-print("The distance at  20 Celsius is {} cm's".format(distance_warm))
-print("The distance at -30 Celsius is {} cm's".format(distance_cold))
-# cleanup gpio pins.
-GPIO.cleanup((trig, echo))
+    #  Create a distance reading with the hcsr04 sensor module
+    value = sensor.Measurement(trig_pin, echo_pin)
+    raw_measurement = value.raw_distance()
+
+    # To overide default room temp you can pass the following to value
+    # This can be combined with a temperature sensor for cold weather apps.
+    # value = sensor.Measurement(trig_pin,
+    #                            echo_pin,
+    #                            temperature=2,
+    #                            )
+
+    # Calculate the distance in centimeters
+    print("The Distance = {} centimeters".format(round(raw_measurement, 1)))
+
+
+if __name__ == "__main__":
+    main()
