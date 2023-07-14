7e6579bb7d5e0bb797363d010cff0d8dab204716
Tobias Krainhöfner <tobikrain@gmail.com>
2023-07-14 10:32:14 +0200

    update

--- test.py
+++ test.py
@@ -1,52 +1,63 @@
 import RPi.GPIO as GPIO
 import time
- 
-#GPIO Modus (BOARD / BCM)
+import random
+
+# GPIO Modus (BOARD / BCM)
 GPIO.setmode(GPIO.BCM)
- 
-#GPIO Pins zuweisen
+
+# GPIO Pins zuweisen
 GPIO_TRIGGER = 18
 GPIO_ECHO = 24
- 
-#Richtung der GPIO-Pins festlegen (IN / OUT)
+
+# Richtung der GPIO-Pins festlegen (IN / OUT)
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
- 
+
 def distanz():
-    # setze Trigger auf HIGH
+    # Setze Trigger auf HIGH
     GPIO.output(GPIO_TRIGGER, True)
- 
-    # setze Trigger nach 0.01ms aus LOW
+
+    # Setze Trigger nach 0.01ms auf LOW
     time.sleep(0.00001)
     GPIO.output(GPIO_TRIGGER, False)
- 
+
     StartZeit = time.time()
     StopZeit = time.time()
- 
-    # speichere Startzeit
+
+    # Speichere Startzeit
     while GPIO.input(GPIO_ECHO) == 0:
         StartZeit = time.time()
- 
-    # speichere Ankunftszeit
+
+    # Speichere Ankunftszeit
     while GPIO.input(GPIO_ECHO) == 1:
         StopZeit = time.time()
- 
-    # Zeit Differenz zwischen Start und Ankunft
+
+    # Zeitdifferenz zwischen Start und Ankunft
     TimeElapsed = StopZeit - StartZeit
-    # mit der Schallgeschwindigkeit (34300 cm/s) multiplizieren
-    # und durch 2 teilen, da hin und zurueck
+    # Mit der Schallgeschwindigkeit (34300 cm/s) multiplizieren
+    # und durch 2 teilen, da hin und zurück
     distanz = (TimeElapsed * 34300) / 2
- 
+
     return distanz
- 
+
 if __name__ == '__main__':
     try:
+        input_wert = int(input("Geben Sie einen Wert ein: "))
+        zufallszahl = random.randint(1, input_wert)
+        print("Zufallszahl:", zufallszahl)
+        time.sleep(5)
+
         while True:
             abstand = distanz()
-            print ("Gemessene Entfernung = %.1f cm" % abstand)
+            print("Gemessene Entfernung = %.1f cm" % abstand)
+            toleranz = zufallszahl * 0.03  # Toleranz von 2-3%
+            if zufallszahl - toleranz <= abstand <= zufallszahl + toleranz:
+                print("Richtige Distanz gehalten!")
+            else:
+                print("Falsche Distanz gehalten. Du musst trinken!")
             time.sleep(1)
- 
-        # Beim Abbruch durch STRG+C resetten
+
+    # Beim Abbruch durch STRG+C zurücksetzen
     except KeyboardInterrupt:
-        print("Messung vom User gestoppt")
+        print("Messung vom Benutzer gestoppt")
         GPIO.cleanup()
